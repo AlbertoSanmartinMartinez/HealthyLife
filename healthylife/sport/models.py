807f1b9c35dee98e5fa8f4a9e3fbe1c10e8698d8
Alberto Sanmartin Martinez <albertosanmartinmartinez@gmail.com>
2017-04-28 11:59:44 +0200

    models created

--- healthylife/sport/models.py
+++ healthylife/sport/models.py
@@ -1,3 +1,27 @@
+from __future__ import unicode_literals
 from django.db import models
 
+
 # Create your models here.
+class sport_sesion(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+    # sport_type =
+    # duration
+    # calories
+    # time
+
+    def __unicode__(self):  # python 2
+        return self.name
+
+    def __str__(self):  # python 3
+        return self.name
+
+
+class sport_type(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+
+    def __unicode__(self):  # python 2
+        return self.name
+
+    def __str__(self):  # python 3
+        return self.name
