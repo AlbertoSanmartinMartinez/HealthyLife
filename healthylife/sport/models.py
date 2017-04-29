792584b98a361a2c15b1d294660dddca637e803c
Alberto Sanmartin Martinez <albertosanmartinmartinez@gmail.com>
2017-04-29 11:37:48 +0200

    statistics app created

--- healthylife/sport/models.py
+++ healthylife/sport/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
 
 
 # Create your models here.
@@ -21,7 +22,7 @@ class SportSesion(models.Model):
     session_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     sport_type = models.ForeignKey(SportType)
-    date = models.DateField()
+    date = models.DateField(date.today)
     usuario = models.ForeignKey(User)
     # duration
     # calories
@@ -32,3 +33,6 @@ class SportSesion(models.Model):
 
     def __str__(self):  # python 3
         return self.name
+
+    def saveSportSesion(self):
+        self.save()
--- healthylife/statistics/models.py
+++ healthylife/statistics/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+from __future__ import unicode_literals
+# from django.db import models
+
 
 # Create your models here.
+'''
+class GeneralStatistics(models.Model):
+    pass
+
+
+class SessionStatistics(models.Model):
+    pass
+'''
