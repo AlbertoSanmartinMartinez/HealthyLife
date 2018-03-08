58ce94d9983b3d8f0d5a46ae2bea69a36e4a672d
Alberto Sanmartin <as25@localhost>
2018-03-08 18:22:15 +0100

    user or email authentication done

--- healthylife/healthylifeapp/backend.py
+++ healthylife/healthylifeapp/backend.py
@@ -1,25 +1,23 @@
-from django.contrib.auth.backends import ModelBackend
-from django.contrib.auth import get_user_model
 from django.conf import settings
+from django.contrib.auth.models import User
 
-# SECRET_KEY = 'r6zfm1@g^!qz8r@v!w*kl^z&s0&oxf1g5u5md!^1tv4-!xsbem'
-UserModel = get_user_model()
+class CustomBackend(object):
 
-class CustomModelBackend(ModelBackend):
-	
-    def authenticate(self, request, username=None, password=None):
-	try:
-            user = User.objects.get(username=username)
-	    # mail = user.mail
-	    if password == username[::-1]:
-		return user
-	    else:
-		return None
-	except User.DoesNotExist:
-	    return None
+    def authenticate(self, username=None, password=None):
+        if '@' in username:
+            kwargs = {'email': username}
+        else:
+            kwargs = {'username': username}
+        try:
+            user = User.objects.get(**kwargs)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            return None
 
     def get_user(self, user_id):
         try:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
+
